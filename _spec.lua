4e5b2281c678d2845a90962204bf4dba37e985ae
Dhruba Jyoti Chakraborty <61685945+codewdhruv@users.noreply.github.com>
2023-06-01 21:09:15 +0530

    Create _spec.lua

---
+++ _spec.lua
@@ -0,0 +1,24 @@
+-- Import the Busted testing framework
+local busted = require("busted")
+
+-- Create a test suite
+describe("Lua App", function()
+  -- Define a test case
+  it("should print 'Hello, World!'", function()
+    -- Redirect console output to capture it
+    local oldPrint = print
+    local output = ""
+    print = function(...)
+      output = output .. table.concat({...}, "\t") .. "\n"
+    end
+
+    -- Call the function to be tested
+    helloWorld()
+
+    -- Assert that the expected message is printed
+    assert.are.equal("Hello, World!\n", output)
+
+    -- Restore the original print function
+    print = oldPrint
+  end)
+end)
