96eda17caad43885b435906c65053f168aadf1bc
Dhruba Jyoti Chakraborty <61685945+codewdhruv@users.noreply.github.com>
2023-06-01 20:57:06 +0530

    Create test.lua

---
+++ test.lua
@@ -0,0 +1,9 @@
+-- This is a Lua comment
+
+-- Define a function that prints "Hello, World!"
+function helloWorld()
+    print("Hello, World!")
+end
+
+-- Call the function to print the message
+helloWorld()
